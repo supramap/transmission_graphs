715a78df760df3903eef409f60d586135fa0c59d
Colby Ford <colbytylerford@gmail.com>
2019-02-10 09:55:10 -0500

    Added favicon

--- strainhub/app.R
+++ strainhub/app.R
@@ -27,6 +27,7 @@ ui <- tagList(
   # tags$head(tags$style(type="text/css", "html, body {width: 100%; height: 100%; overflow: hidden}")),
   navbarPage(
     theme = shinytheme("flatly"),
+    #theme = "uncc.css",
     title = "StrainHub",
     tabPanel("Network Visualizer",
              sidebarPanel(
